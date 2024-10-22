ff078ff6156483982f997177f2ba24334b0f7589
Colin Smith <colinsmith@smithcolin.co.uk>
2024-10-22 10:03:57 +0100

    added env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const router = require('./router');
 app.use(express.json());
 app.use(router);
